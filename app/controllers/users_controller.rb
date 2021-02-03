f851fe00d43721bf56dce0613f6b0d8e0de3e8ff
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-03 16:49:41 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,19 @@
 class UsersController < ApplicationController
-  def home
-    @user = User.find_by(id: session[:user_id])
-    redirect_to '/login' unless @user.present?
-  end
 
-  def new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      redirect_to "/"
-    else
-      flash[:error] = "Try again!"
-      redirect_to "/users/new"
+    def new
     end
-  end
 
-  private
+    def create
+        @user = User.create(user_params)
+        return redirect_to controller: 'users', action: 'new' unless @user.save
+        session[:user_id] = @user.id
+        redirect_to controller: "welcome", action: "home"
+    end
 
-  def user_params
-    params.require(:user).permit(:name, :password, :password_confirmation)
-  end
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
+
 end
