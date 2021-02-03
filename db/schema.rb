50847c74969ac718fcb26d6afda2a734084bd841
Robert Clarke <r.jason.clarke@gmail.com>
2021-02-03 16:53:03 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210202224602) do
+ActiveRecord::Schema.define(version: 20210202032153) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
